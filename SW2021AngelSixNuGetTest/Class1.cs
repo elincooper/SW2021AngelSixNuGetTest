e75e2b1036c245ddbb48b0d235b8e0e03c7b8e5a
Ewald Kober <Ewald.Kober@elinmotoren.at>
2021-09-28 08:53:46 +0200

    first update

--- SW2021AngelSixNuGetTest/Class1.cs
+++ SW2021AngelSixNuGetTest/Class1.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AngelSix.SolidDna;
+using Dna;
+using static AngelSix.SolidDna.SolidWorksEnvironment;
 
 namespace SW2021AngelSixNuGetTest
 {
-    public class Class1
+    public class SolidDnaAddinIntegration : AddInIntegration
     {
+        public override void ApplicationStartup()
+        {
+            
+        }
+
+        public override void PreConnectToSolidWorks()
+        {
+            
+        }
+
+        public override void PreLoadPlugIns()
+        {
+            
+        }
+    }
+
+    public class NuGetTestPlugIn : SolidPlugIn
+    {
+        public override string AddInTitle => "NuGetTestPlugIn";
+
+        public override string AddInDescription => "Test the NuGet package";
+
+        public override void ConnectedToSolidWorks()
+        {
+            Application.ShowMessageBox("test", SolidWorksMessageBoxIcon.Information);
+        }
+
+        public override void DisconnectedFromSolidWorks()
+        {
+
+        }
     }
 }
